e3ccdecc52f3efd6a4543f1a581b97ac242df1e9
Nguyen Minh Tri <tri.nguyen@naivecat.com>
2021-06-09 02:51:29 +0700

    Add ChakraProvider from ChakraUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default MyApp;
