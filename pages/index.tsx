7802e1d24d7da434536ac6bb35c82803104671fe
Nguyen Minh Tri <tri.nguyen@naivecat.com>
2021-06-09 16:58:51 +0700

    Fix Type error: Object is possibly 'undefined' when using refs

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { useRef } from "react";
 import { Box, Input, Link } from "@chakra-ui/react";
 
 const Home = () => {
-  const childRef = useRef();
+  const childRef = useRef(null);
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.value);
