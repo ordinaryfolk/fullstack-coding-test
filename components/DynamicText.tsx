723a83dccf811cfdc9efa1496f6cce54ba488bac
richardpb <rpbeavis@gmail.com>
2022-09-27 22:46:13 +0900

    feat: user input updates text value within the comDynamicText component

--- components/DynamicText.tsx
+++ components/DynamicText.tsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, forwardRef, useImperativeHandle } from "react";
 import { Heading } from "@chakra-ui/react";
 
-const DynamicText = () => {
+const DynamicText = forwardRef((props, ref) => {
   const [value, setValue] = useState("Random Text");
 
-  const changeValue = (newValue) => {
-    setValue(newValue);
-  };
+  const changeValue = (newValue) => setValue(newValue);
+
+  useImperativeHandle(ref, () => ({ changeValue: changeValue }));
 
   return <Heading>{value}</Heading>;
-};
+});
 
 export default DynamicText;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import React, { useRef } from "react";
+
 /* Components */
 import Head from "next/head";
 import DynamicText from "../components/DynamicText";
@@ -7,24 +9,25 @@ import { Flex, Box, Input } from "@chakra-ui/react";
 import styles from "../styles/Home.module.css";
 
 const Home = () => {
+  const dynamicTextRef = useRef(null);
+
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.value);
+    dynamicTextRef.current.changeValue(e.target.value || "Random Text");
   };
 
-  return (   
+  return (
     <Flex className={styles.container}>
       <Head>
         <title>Coding Test</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Flex direction="column" p={12} rounded={6}>
-
+        <Box className={styles.main}>
+          <DynamicText ref={dynamicTextRef}/>
+          <Input placeholder="input some text..." onChange={onChange} />
+        </Box>
       </Flex>
-
-      <Box className={styles.main}>
-        <DynamicText />
-        <Input placeholder="input some text..." onChange={onChange} />
-      </Box>
     </Flex>
   );
 };
